190b10dec1cdd9720c96b59fff9379663f44de60
maksimiliani <40344223+maksimiliani@users.noreply.github.com>
2022-08-31 21:17:11 -0600

    Update crosspage_new.js

--- js/crosspage_new.js
+++ js/crosspage_new.js
@@ -1,4 +1,4 @@
-$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/maksimiliani/corti/css/crosspage-style.css">');
+$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/maksimiliani/corti@57633ee/css/crosspage-style.css">');
 
 var sections_color;
 var locked = false;
